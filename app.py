f416c3e16dd531d26150f5528543676e89bd36a1
ryszyy <ryszyy@gmail.com>
2017-06-02 11:13:06 +0200

    adding comment added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import random
 import re
 import sys
 import time
@@ -21,8 +22,9 @@ def main():
         context["url"]=u
 
         driver = webdriver.Chrome()
+        driver.maximize_window()
         driver.get(u)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script("window.scrollTo(0, 600);")
         time.sleep(3)
         while True:
             try:
@@ -44,15 +46,86 @@ def main():
 
         r = Rake()
         r.extract_keywords_from_text(comments_rake)
-        half =len(r.get_ranked_phrases())/2
+        half =len(r.get_ranked_phrases())/3
         keywords_list = r.get_ranked_phrases()[:int(half)]
+        print(keywords_list)
         context['keywords'] = keywords_list
         context['comments'] = comments
+        your = random.choice(keywords_list)
+        context['your'] = your
 
-        return render_template('index.html', context=context)
+        return render_template('comments.html', context=context)
 
     return render_template('index.html', context=context)
 
+
+@app.route("/send", methods=['GET','POST'])
+def send():
+    msg = "Thank you!"
+    context = {}
+    context['msg'] = msg
+    if request.method == 'POST':
+        msg = request.form['comment']
+        context['msg'] = msg
+        # driver = webdriver.PhantomJS('/usr/bin/phantomjs')
+        try:
+            driver = webdriver.Chrome()
+        except:
+            driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get('https://www.youtube.com/watch?v=mg7netw1JuM')
+        youtube_username = "commentator951@gmail.com"
+        youtube_password = "comment951"
+        time.sleep(1)
+        try:
+            driver.find_element_by_xpath('//*[@id="yt-masthead-signin"]/div/button/span').click()
+        except:
+            driver.find_element_by_xpath('//*[@id="buttons"]/ytd-button-renderer[2]/a').click()
+
+        # signin_button.click()
+
+        #login
+        wait_and_find(driver, '//*[@id="identifierId"]', youtube_username)
+        next_button_id = driver.find_element_by_xpath('//*[@id="identifierNext"]/content')
+        next_button_id.click()
+
+        #password
+        wait_and_find(driver, '//*[@id="password"]/div[1]/div/div[1]/input', youtube_password)
+        next_button_pass = driver.find_element_by_xpath('//*[@id="passwordNext"]/content/span')
+        next_button_pass.click()
+
+        time.sleep(4)
+        driver.execute_script("window.scrollTo(0, 600);")
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="comment-section-renderer"]/div[1]/div[1]/div[2]/div[1]').click()
+
+        
+        wait_and_find(driver, '//*[@id="comment-simplebox"]/div[2]/div[2]', msg)
+        send_button = driver.find_element_by_xpath('//*[@id="comment-simplebox"]/div[3]/div[2]/button[2]')
+        send_button.click()
+
+        
+        try:
+            message = driver.find_element_by_xpath('//*[@id="comment-section-renderer-items"]/section[1]/div[1]/div[2]/div[2]/div[1]').get_attribute('innerHTML')
+        except:
+            context['result'] = "Error ocurred"
+        else:
+            context['result'] = f'"{message}" has been sent'
+        driver.close()
+
+        return render_template('comments.html', context=context)
+    return render_template('index.html', context=context)
+
+def wait_and_find(driver, element, text=None):
+    while True:
+        try:
+            found = driver.find_element_by_xpath(element)
+            found.send_keys(text)
+            break
+        except Exception as e:
+            print(e)
+            time.sleep(1)
+            continue
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -3,49 +3,46 @@ import sys
 import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait as wait
-# from rake_nltk import Rake
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.action_chains import ActionChains
-# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def get_comment():
-	# # driver = webdriver.PhantomJS('/usr/bin/phantomjs')
-	# driver = webdriver.Chrome()
-	# url = sys.argv[1]
-	# driver.get(url)
-	# driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-	# time.sleep(3)
-	# while True:
-	# 	try:
-	# 		driver.find_element_by_id("comment-section-renderer-items")
-	# 		break
-	# 	except:
-	# 		time.sleep(3)
-	# 		continue
+	word_list = []
+    #raw data
+    source_code = requests.get(url)
+    #convert to text
+    plain_text = source_code.text
+    #lxml format
+    soup = BeautifulSoup(plain_text,'lxml')
 
-	# comments = ""
-	# for item in driver.find_elements_by_class_name("comment-renderer-text-content"):
-	# 	comments = comments + item.text
-	# 	# print(item.text)
-	# 	# print("-" * 80)
-	# driver.close()
-	# # print(comments)
+    #find the words in paragraph tag
+    for text in soup.findAll('p'):
+        if text.text is None:
+            continue
+        #content
+        content = text.text
+        #lowercase and split into an array
+        words = content.lower().split()
 
-	# r = Rake()
-	# r.extract_keywords_from_text(comments)
-	# half =len(r.get_ranked_phrases())/2
-	# keywords_list = r.get_ranked_phrases()[:int(half)]
-	# print(keywords_list)
-	driver = webdriver.Chrome()
-	driver.get('https://www.youtube.com/')
-	youtube_username = "commentator951@gmail.com"
-	youtube_password = "comment951"
-	'//input[@type="email"]'
-	signin_button_class = 'signin-container '
-	username_id = 'identifierId'
-	'''
-<input type="email" class="whsOnd zHQkBf" jsname="YPqjbf" autocomplete="username" spellcheck="false" tabindex="0" aria-label="Email or phone" name="identifier" id="identifierId" dir="ltr" data-initial-dir="ltr" data-initial-value="" badinput="false">
-	'''
+        #for each word
+        for word in words:
+            #remove non-chars
+            cleaned_word = clean_word(word)
+            #if there is still something there
+            if len(cleaned_word) > 0:
+                #add it to our word list
+                word_list.append(cleaned_word)
+
+    return word_list
+
+def wait_and_find(driver, element, text=None):
+	while True:
+		try:
+			found = driver.find_element_by_xpath(element)
+			found.send_keys(text)
+			break
+		except Exception as e:
+			print(e)
+			time.sleep(1)
+			continue
 
 
 if __name__ == "__main__":
